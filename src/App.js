9bc006135f077961e777d0e4ae192911ad9b1f20
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-08-09 10:45:24 +0530

    Input bar is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import TopButton from './components/TopButton.jsx';
+import Inputs from './components/Inputs.jsx'
 
 function App() {
   return (
-  <div>
-  
+  <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <TopButton />
+    <Inputs />
   </div>
     
   );
