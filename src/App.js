5b78f79b3b15297bdc5c9e1ed8124668ee79cb69
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-08-09 11:03:08 +0530

    Time and location is added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import TopButton from './components/TopButton.jsx';
-import Inputs from './components/Inputs.jsx'
+import Inputs from './components/Inputs.jsx';
+import TimeAndLocation from './components/TimeAndLocation.jsx'
 
 function App() {
   return (
   <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
     <TopButton />
     <Inputs />
+    <TimeAndLocation />
   </div>
     
   );
