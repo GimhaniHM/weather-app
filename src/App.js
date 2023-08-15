720ebe015fd577387e780ec903e908f7ec488173
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-08-16 04:27:43 +0530

    background color change is completed

--- src/App.js
+++ src/App.js
@@ -27,9 +27,18 @@ function App() {
 
   }, [query, units]);
 
+  const changeBackground = () => {
+    if (!weather) return 'from-cyan-700 to-blue-700'
+  
+    const point = units === 'metric' ? 20 : 60;
+
+    if (weather.temp <= point) return 'from-yellow-700 to-orange-700'
+    
+  }
+
 
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ${changeBackground()}`}>
       <TopButton setQuery={setQuery}/>
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
